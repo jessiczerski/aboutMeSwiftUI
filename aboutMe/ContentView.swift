59c4e414e1892fdcf1929a7cbe80838a5834ac29
jessiczerski <jjaneczerski8494@gmail.com>
2023-04-22 00:53:43 -0400

    Initial Commit

---
+++ aboutMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aboutMe
+//
+//  Created by JESSI on 4/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aboutMe/aboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeApp.swift
+//  aboutMe
+//
+//  Created by JESSI on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
